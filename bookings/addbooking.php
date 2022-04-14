265478a25729c223952f1a8f64d3c2e3380a1e9e
ErblinRexhepi <er53677@ubt-uni.net>
2022-04-14 09:03:29 -0700

    booking CRUD

---
+++ bookings/addbooking.php
@@ -0,0 +1,47 @@
+<?php
+include '../databaseConnection/db.php'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST">
+    <input type="text" name="name" class="form" placeholder="name">
+    <input type="text" name="surname" class="form"  placeholder="surname">
+    <input type="text" name="email" class="form"  placeholder="email">
+    <input type="text" name="username" class="form"  placeholder="username">
+    <input type="text" name="phone_number" class="form"  placeholder="phone number">
+    <input type="text" name="oferta" class="form"  placeholder="oferta">
+    <input type="submit" value="submit" name="submit">
+</form>
+
+
+<?php
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $phonenumber = $_POST['phone_number'];
+    $oferta = $_POST['oferta'];
+    
+    $qry ="INSERT INTO bookings values(null,'$name','$surname','$email','$username','$phonenumber','$oferta')";
+    if(mysqli_query($conn, $qry)){
+        echo'<script type="text/javascript">
+          alert("booking succesfully")
+          window.location="../dashboard/dashboard.php"
+          </script>';
+    }
+    else{
+        echo "error";
+    }
+}
+?>
+</body>
+</html>
--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -84,3 +84,43 @@ include "../databaseconnection/db.php";
 
 ?>      
 </table> 
+<h3>bookings</h3>
+    <table border="1">
+    <tr>
+        <th>id</th>
+        <th>name</th>
+        <th>surname</th>
+        <th>email</th>
+        <th>username</th>
+        <th>phone number</th>
+        <th>oferta</th>
+        <th>actions</th>
+    </tr>
+    <?php
+       $sql = "SELECT * FROM booking";
+       $run = $conn->query($sql);
+       if($run ->num_rows > 0){
+           while($row = $run ->fetch_assoc()){   
+    ?>
+    <tr>
+        <td><?php echo $row['id']; ?></td>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['surname']; ?></td>
+        <td><?php echo $row['email']; ?></td>
+        <td><?php echo $row['username']; ?></td>
+        <td><?php echo $row['phone_number']; ?></td>
+        <td><?php echo $row['oferta']; ?></td>
+        <td>
+            <a href="../booking/addbooking.php">add</a>
+            <a href="../bookings/editbooking.php?id=<?php echo $row['id']; ?>">edit</a>
+            <a href="deletuser.php?delete_id=<?php echo $row['id']; ?>">delete</a>
+        </td>
+    </tr>
+    <br>
+    
+<?php
+   }
+} 
+
+     ?>
+      </table> <br>
